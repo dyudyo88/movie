d2c2418715e16c484b52c630e2b157fd859e7e7b
김주홎 <gyong0812@naver.com>
2018-12-04 21:20:40 +0900

    동적메모리할당

--- movie.c
+++ movie.c
@@ -11,13 +11,15 @@ typedef struct movInfo {
 	char madeIn[10];
 } movInfo_t;
 
+int mvPtr; 
 void* mv_genMvInfo(char* name, float score, int runTime, char* country)
 {
 	movInfo_t* mvPtr;
-	
 	//allocate memory and set the member variables
+	mvPtr = (movInfo_t*)malloc(sizeof(movInfo_t)); //동적메모리 할당->malloc함수 이용  
 	
-	return (void*)mvPtr;
+	
+	return (void*)mvPtr; 
 }
 
 void printMv(void* obj)
@@ -36,24 +38,62 @@ void printMv(void* obj)
 
 int mv_printAll(void* obj, void* arg)
 {
+	movInfo_t* mvPtr = (movInfo_t*)obj;
+	if (mvPtr == NULL)
+	{
+		printf("[ERROR] failed to print the movie Info! (object is NULL)\n");
+	}
 	
+	printf("Name : %s (%s)\n", mvPtr->name, mvPtr->madeIn);
+	printf("running time : %i, score : %f\n", mvPtr->runTime, mvPtr->score);
+	
+	return;
 }
 
-int mv_printScore(void* obj, void* arg)
+ 
+/*int mv_printScore(void* obj, void* arg)
 {
+	movInfo_t* mvPtr = (movInfo_t*)obj;
+	if (mvPtr == NULL)
+	{
+		printf("[ERROR] failed to print the movie Info! (object is NULL)\n");
+	}
 	
+	
+	printf("Name : %s (%s)\n", mvPtr->name, mvPtr->madeIn);
+	printf("running time : %i, score : %f\n", mvPtr->runTime, mvPtr->score);
+	
+	return;
 }
 
 int mv_printRunTime(void* obj, void* arg)
 {
+	movInfo_t* mvPtr = (movInfo_t*)obj;
+	if (mvPtr == NULL)
+	{
+		printf("[ERROR] failed to print the movie Info! (object is NULL)\n");
+	}
 	
+	printf("Name : %s (%s)\n", mvPtr->name, mvPtr->madeIn);
+	printf("running time : %i, score : %f\n", mvPtr->runTime, mvPtr->score);
+	
+	return;
 }
 
 int mv_printCountry(void* obj, void* arg)
 {
+	movInfo_t* mvPtr = (movInfo_t*)obj;
+	if (mvPtr == NULL)
+	{
+		printf("[ERROR] failed to print the movie Info! (object is NULL)\n");
+	}
 	
+	printf("Name : %s (%s)\n", mvPtr->name, mvPtr->madeIn);
+	printf("running time : %i, score : %f\n", mvPtr->runTime, mvPtr->score);
+	
+	return;
 }
 
-
+*/
 
 
