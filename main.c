4959dc2e50857cf60cc2cacfb366a9a46102ad7a
김주홎 <gyong0812@naver.com>
2018-12-09 23:00:40 +0900

    끝

--- main.c
+++ main.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "movie.h"
-#include "linkedList.h"
+#include "movie.h" 								//movie.c의 헤더  
+#include "linkedList.h" 						//linkedList의 헤더  
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	
-	FILE *fp; //FILE pointer for reading movie data 
-	char name[200]; //movie name
-	char country[10]; //movie country
-	int runTime; //movie runtime
-	float score; //movie score
+	FILE *fp; 									//FILE pointer for reading movie data 
+	char name[200]; 							//movie name
+	char country[10]; 							//movie country
+	int runTime; 								//movie runtime
+	float score; 								//movie score
 	
-	int exit_flag = 0; //flag variable for while loop
-	int option; //user input option
-	void *list, *mvInfo; //pointers for linked list and a specific structure instance for a movie data
-	int (*repFunc)(void* obj, void* arg); //function pointer for using list_repeatFunc() function
-	void *arg; //a void pointer for passing argument to repFunc
-	int cnt; //integer variable
+	int exit_flag = 0; 							//flag variable for while loop
+	int option; 								//user input option
+	void *list, *mvInfo;						//pointers for linked list and a specific structure instance for a movie data
+	int (*repFunc)(void* obj, void* arg); 		//function pointer for using list_repeatFunc() function
+	void *arg; 									//a void pointer for passing argument to repFunc
+	int cnt; 									//integer variable
+	
+	
 	
 	//1. reading the movie.dat-----------------------------
 	printf("Reading the data files...\n");
 	//1.1 FILE open
-	fp = fopen("movie.dat","r");
+	fp = fopen("movie.dat","r"); //영화 DB파일 열기  
 	
 	
 	//1.2 list generation (use function list_genList() )
@@ -36,101 +39,102 @@ int main(int argc, char *argv[]) {
 		return 1; // 파일 오픈을 실패했으므로 return 1
 	}
 	
-	
-	
+
 	//1.3 read each movie data from the file and add it to the linked list
-	while (fscanf(fp, "%s %s %d %f", name, country, &runTime, &score) != EOF) //파일 정보를 입력 받음  
+	while (fscanf(fp, "%s %s %d %f", name, country, &runTime, &score) != EOF) //파일 정보를 입력 받음 
 	{	
 		//generate a movie info instance(mvInfo) with function mv_genMvInfo()
-		mvInfo = mv_genMvInfo(name, score, runTime, country); //mvInfo에다가 my_genMvInfo 정보 대입  
-		list_addTail(mvInfo, list);
+		mvInfo = mv_genMvInfo(name, score, runTime, country); //mvInfo에다가 my_genMvInfo 정보 대입(영화 제목, 평점, 상영시간, 제작 국가)
+		list_addTail(mvInfo, list);  //addTail node 
 	}
 	
 	
-	printf("Read done! %d items are read! \n\n",list_len(list)); //몇 개의 영화가 있는지 출력  
+	printf("Read done! %d items are read! \n\n",list_len(list)); //몇 개의 영화가 읽혔는지 출력  
 	
 	
 	//1.4 FILE close 파일 닫기  
-	fclose(fp);  
+	fclose(fp);  //파일 닫기  
 	
 	//2. program start
 	while(exit_flag == 0)
 	{
 		//2.1 print menu message and get input option
 		printf("-------------------- Menu --------------------\n");
-		printf("1. print all the movies\n");
-		printf("2. search for specific country movies\n");
-		printf("3. search for specific runtime movies\n");
-		printf("4. search for specific score movies\n");
-		printf("5. exit\n");
+		printf("1. print all the movies\n"); 						//옵션1 : 모든 영화 DB출력  
+		printf("2. search for specific country movies\n"); 			//옵션2 : 특정 제작국가 영화 DB출력   
+		printf("3. search for specific runtime movies\n"); 			//옵션3 : 특정 상영시간  DB출력  
+		printf("4. search for specific score movies\n");			//옵션4 : 특정 영화 평점  DB출력 
+		printf("5. exit\n"); 										//옵션5 : 프로그램 끝내기(나가기) 
 		printf("-------------------- Menu --------------------\n\n");
-		printf("-- select an option :");
-		scanf("%d",&option);
+		printf("-- select an option :"); 							//선택할 옵션  
+		scanf("%d",&option); 										//선택할 옵션 입력받기  
 		
-		switch(option)
+		switch(option) //선택한 옵션에 따라서 프로그램 실행 및 출력 하기  
 		{	
-			case 1: //print all the movies
-				printf("\nprinting all the movies in the list.....\n\n\n");
-				printf("----------------------------------------\n");
+			case 1: //print all the movies ->옵션 1을 선택했을 때  
+				printf("\nprinting all the movies in the list.....\n\n\n"); 
+				printf("------------------------------------------------------\n");
 				
 				repFunc = mv_printAll;	//repFunc에 매개변수에 만들었던 함수 넣기, mv_printAll 함수 넣음->전부 다 출력
-				arg = NULL;				//arg 필요하지 않으므로 null 
+				arg = NULL;				//arg가 필요하지 않으므로 null 
 				
-				exit_flag = 0; //exit_flag = 1 일때 함수가 끝남 
+				exit_flag = 0; 			//exit_flag = 1 일때 함수가 끝남 
 				break;
 				
-			case 2: //print movies of specific country
-				printf("select a country :");
-				scanf("%s",&country);
+			case 2: //print movies of specific country ->옵션 2를 선택했을 때  
+				printf("select a country :");  
+				scanf("%s",&country); 		//제작국가 입력받기  
 				printf("------------------------------------------------------\n");
-				repFunc = mv_printCountry; //repFunc에 매개변수에 만들었던 함수 넣기,mv_printCountry 함수 넣음
+				repFunc = mv_printCountry;  //repFunc에 매개변수에 만들었던 함수 넣기,mv_printCountry 함수 넣음
 				arg = country;				//arg 에 Country넣기(영화 제작 국가) 
 				
-				exit_flag = 0; //exit_flag = 1 일때 함수가 끝남
-				break;
+				exit_flag = 0; 				//exit_flag = 1 일때 함수가 끝남
+				break;						//switch문 종료  
 				
-			case 3: //print movies with long runtime
+			case 3: //print movies with long runtime ->옵션 3을 선택 했을 때  
 				printf("lowest runtime :");
-				scanf("%d",&runTime);
+				scanf("%d",&runTime);		//최소 상영시간 입력받기  
 				printf("------------------------------------------------------\n");
 				repFunc = mv_printRunTime; //repFunc에 매개변수에 만들었던 함수 넣기,mv_printRunTime 함수 넣음
-				arg = &runTime;				//arg 에 runtime넣기(영화의 최소 상영시간 입력받는 것 ) 
+				arg = &runTime; //arg 에 runtime넣기(영화의 최소 상영시간 입력받는 것 ) 
 				
 				exit_flag = 0;	//exit_flag = 1 일때 함수가 끝남 
-				break;
+				break;			//switch문 종료    
 				
-			case 4: //print movies with high score
+			case 4: //print movies with high score ->옵션 4를 선택했을 때  
 				printf("lowest score :");
-				scanf("%f",&score);
+				scanf("%f",&score);			//최소 평점 입력 받기  
 				printf("------------------------------------------------------\n");
 				repFunc = mv_printScore; //repFunc에 매개변수에 만들었던 함수 넣기,mv_prinScore 함수 넣음
-				arg = &score;				//arg 에 score넣기(영화의 최소 평점  입력받는 것 ) 
+				arg = &score;//arg 에 score넣기(영화의 최소 평점  입력받는 것 ) 
 				
 				exit_flag = 0;	//exit_flag = 1 일때 함수가 끝남 
-				break;
+				break;			//switch문 종료  
 				
 			case 5:
 				printf("\n\n   Bye!\n\n");
 				exit_flag = 1;	//함수가 끝남 
-				break;
+				break;	//switch문 종료  
 				
 			default:
-				printf("wrong command! input again\n");
+				printf("wrong command! input again\n"); //1~5까지의 옵션 중 선택한 문자가 아닐 때 출력하는 것   
 				exit_flag = 0; //exit_flag = 1 일때 함수가 끝남  
-				break;
+				break;	//switch문 종료  
 		}
 		
 		
 		//2.2 printing operation by function pointer (list_repeatFunc() is called here)
+		
 		if(1<=option && option<=4) //option을 1~4까지 선택했을 때만 몇 개의 영화가 출력되었는지 출력하도록 하기 
 		{	
-			cnt = list_repeatFunc(repFunc, arg, list); //교수님이 알려주신 힌트 		
+			cnt = list_repeatFunc(repFunc, arg, list); //몇 개의 영화리스트가 출력(검색) 되었는지 알려주는 함수  		
 			//2.3 print number of movies
-			printf("\n    - totally %d movies are listed!\n\n\n", cnt); 
+			printf("\n    - totally %d movies are listed!\n\n\n", cnt);  //몇 개의 영화 리트스가 검색되었는지 출력  
 		}
+		
 	}
 	
-	return 0;
+	return 0;  
 }
 
 
--- movie.c
+++ movie.c
@@ -5,56 +5,60 @@
 
 //structure definition 구조체 정의  
 typedef struct movInfo //struct의 다른 이름을 "movInfo"로 하겠다. 
- {
-	char name[100]; //영화 이름 
-	float score;	//영화 평점  
-	int runTime;	//영화 상영시간  
+{
+	char name[100]; 	//영화 이름 
+	float score;		//영화 평점  
+	int runTime;		//영화 상영시간  
 	char madeIn[10];	//영화 제작 국가  
-}  movInfo_t; //구조체 변수선언  
+}movInfo_t; 			//구조체 변수선언  
+
 
 int mvPtr; //포인터로 사용 할 변수 선언하기  
 
+
 void* mv_genMvInfo(char* name, float score, int runTime, char* country)
 {
-	movInfo_t* mvPtr; //구조체 변수 movInfo_t의 포인터mvPtr를 선언함   
+	movInfo_t* mvPtr;//구조체 변수 movInfo_t의 포인터mvPtr를 선언함   
 	//allocate memory and set the member variables
 	mvPtr = (movInfo_t*)malloc(sizeof(movInfo_t)); //동적메모리 할당
-	strcpy(mvPtr->name, name); 		//문자열 복사 strcpy(대상, 원본)->파일에서 가져온 name정보를 포인터로 name에 저장함 
-	strcpy(mvPtr->madeIn, country);//문자열 복사 strcpy(대상, 원본)->파일에서 가져온 country정보를 포인터로madeIn에 저장함  
+	strcpy(mvPtr->name, name); 	//문자열 복사 strcpy(대상, 원본)->파일에서 가져온 name정보를 포인터로 name에 저장함 
+	strcpy(mvPtr->madeIn, country);	//문자열 복사 strcpy(대상, 원본)->파일에서 가져온 country정보를 포인터로madeIn에 저장함  
 	//파일에서 가져온 score 정보랑 runTime 정보를 가져와서 저장해야 됨.
-	mvPtr->score = score;
-	mvPtr->runTime = runTime;
+	mvPtr->score = score; //파일에서 가져온 score 정보를 score에 저장  
+	mvPtr->runTime = runTime; //파일에서 갖온 runTime 정보를 runTime 에 저장  
 	
 	return (void*)mvPtr; //(void*)mvPtr반환 해주기  
 }
 
 void printMv(void* obj)
 {
-	movInfo_t* mvPtr = (movInfo_t*)obj;
+	movInfo_t* mvPtr = (movInfo_t*)obj; //mvPtr에 obj 대입 
+	
 	if (mvPtr == NULL) //mvPtr이 NULL일 때 출력  
 	{
 		printf("[ERROR] failed to print the movie Info! (object is NULL)\n"); 
 		return 0; 
 	}
 	
-	printf("Name : %s (%s)\n", mvPtr->name, mvPtr->madeIn);
-	printf("running time : %i, score : %f\n", mvPtr->runTime, mvPtr->score);
+	printf("Name : %s (%s)\n", mvPtr->name, mvPtr->madeIn); //영화 이름, 제작국가 출력  
+	printf("running time : %i, score : %f\n", mvPtr->runTime, mvPtr->score); //영화 상영시간, 평점 출력  
 	printf("------------------------------------------------------\n");
 	
-	return 0; // 노드는 끝나지 않음
+	return 0; // 노드는 끝나지 않음 
 }
 
 int mv_printAll(void* obj, void* arg)
 {
 	movInfo_t* mvPtr = (movInfo_t*)obj; //mvPtr에 obj 대입 
-	if (mvPtr == NULL)
+	
+	if (mvPtr == NULL) //mvPtr이 NULL일 때 출력  
 	{
 		printf("[ERROR] failed to print the movie Info! (object is NULL)\n");
-		return 0; 
+		return 0;  
 	}
 	
-	printf("Name : %s (%s)\n", mvPtr->name, mvPtr->madeIn);
-	printf("running time : %i, score : %f\n", mvPtr->runTime, mvPtr->score);
+	printf("Name : %s (%s)\n", mvPtr->name, mvPtr->madeIn); //영화 이름, 제작국가 출력  
+	printf("running time : %i, score : %f\n", mvPtr->runTime, mvPtr->score); //영화 상영시간, 평점 출력  
 	printf("------------------------------------------------------\n");
 	return 1; //return 1 : 노드는 끝남. 
 }
@@ -63,7 +67,8 @@ int mv_printAll(void* obj, void* arg)
 int mv_printScore(void* obj, void* arg)
 {
 	movInfo_t* mvPtr = (movInfo_t*)obj; //mvPtr에 obj 대입  
-	if (mvPtr == NULL)
+	
+	if (mvPtr == NULL) //mvPtr이 NULL일 때 출력  
 	{
 		printf("[ERROR] failed to print the movie Info! (object is NULL)\n");
 		return 0; //return 0 : 노드는 끝나지 않음
@@ -72,20 +77,22 @@ int mv_printScore(void* obj, void* arg)
 	if( (mvPtr->score) > *(float*)(arg) )//입력한 것 보다 높은 평점을 가진 영화를 출력해야 됨
 										//void*형을 float*형으로 변환하기 + mvPtr->score과 비교하기    
 	{
-		printf("Name : %s (%s)\n", mvPtr->name, mvPtr->madeIn);
-		printf("running time : %i, score : %f\n", mvPtr->runTime, mvPtr->score);
+		printf("Name : %s (%s)\n", mvPtr->name, mvPtr->madeIn); //영화 이름, 제작국가 출력  
+		printf("running time : %i, score : %f\n", mvPtr->runTime, mvPtr->score); //영화 상영시간, 평점 출력  
 		printf("------------------------------------------------------\n");
 		return 1; //return 1 : 노드는 끝남. 
 	}
 	
 	return 0;  //return 0 : 노드는 끝나지 않음
-				// return의 값은 출력할 때만 증가시키는 것 이니까 출력하지 않으면 0을 리턴  
+				// return의 값은 출력할 때만 증가시키는 것 이니까 출력하지 않으면 0을 리턴 
+				// 전체 출력이니까 출력 할 때마다 리턴해서 1을 증가 시킴  
 }
 
 int mv_printRunTime(void* obj, void* arg)
 {
 	movInfo_t* mvPtr = (movInfo_t*)obj; //mvPtr에 obj 대입 
-	if (mvPtr == NULL)
+	
+	if (mvPtr == NULL) //mvPtr이 NULL일 때 출력  
 	{
 		printf("[ERROR] failed to print the movie Info! (object is NULL)\n");
 		return 0;  //return 0 : 노드는 끝나지 않음
@@ -93,36 +100,38 @@ int mv_printRunTime(void* obj, void* arg)
 	
 	if( (mvPtr->runTime) > *(int*)(arg) ) //void*형을 int*형으로 변환하기 + mvPtr->runTime과 비교하기  
 	{
-		printf("Name : %s (%s)\n", mvPtr->name, mvPtr->madeIn);
-		printf("running time : %i, score : %f\n", mvPtr->runTime, mvPtr->score);
+		printf("Name : %s (%s)\n", mvPtr->name, mvPtr->madeIn); //영화 이름, 제작국가 출력  
+		printf("running time : %i, score : %f\n", mvPtr->runTime, mvPtr->score); //영화 상영시간, 평점 출력  
 		printf("------------------------------------------------------\n");
 		
 		return 1; //return 1 : 노드는 끝남.  
 	}
 	
 	return 0;  //return 0 : 노드는 끝나지 않음
-				// return의 값은 출력할 때만 증가시키는 것 이니까 출력하지 않으면 0을 리턴  
+				// return의 값은 출력할 때만 증가시키는 것 이니까 출력하지 않으면 0을 리턴 
+				// 전체 출력이니까 출력 할 때마다 리턴해서 1을 증가 시킴
 }
 
 int mv_printCountry(void* obj, void* arg)
 {
-	movInfo_t* mvPtr = (movInfo_t*)obj;
-	if (mvPtr == NULL)
+	movInfo_t* mvPtr = (movInfo_t*)obj; //mvPtr에 obj 대입 
+	if (mvPtr == NULL) //mvPtr이 NULL일 때 출력  
 	{
 		printf("[ERROR] failed to print the movie Info! (object is NULL)\n");
-		return 0;  //return 0 : 노드는 끝나지 않음
+		return 0;  	//return 0 : 노드는 끝나지 않음
 	}
 	
 	if( strcmp(mvPtr->madeIn , arg) == 0)   //mvPtr->madeIn 과 arg(Country) 문자열을 비교해서 같은지 비교하는 것,문자열이 같으면 0이 반환된다. 
 	{
-		printf("Name : %s (%s)\n", mvPtr->name, mvPtr->madeIn);
-		printf("running time : %i, score : %f\n", mvPtr->runTime, mvPtr->score);
-		printf("------------------------------------------------------\n");
+		printf("Name : %s (%s)\n", mvPtr->name, mvPtr->madeIn); 				 //영화 이름, 제작국가 출력  
+		printf("running time : %i, score : %f\n", mvPtr->runTime, mvPtr->score); //영화 상영시간, 평점 출력  
+		printf("------------------------------------------------------\n"); 
 		return 1; //return 1 : 노드는 끝남.  
 	}
 	
-	return 0;  //return 0 : 노드는 끝나지 않음
-				// return의 값은 출력할 때만 증가시키는 것 이니까 출력하지 않으면 0을 리턴  
+	return 0;   //return 0 : 노드는 끝나지 않음
+				// return의 값은 출력할 때만 증가시키는 것 이니까 출력하지 않으면 0을 리턴
+				// 전체 출력이니까 출력 할 때마다 리턴해서 1을 증가 시킴  
 }
 
 
